9759a9f8abea3ae613a6afc917c3cd8a42836286
geicof <fgahs1202@gmail.com>
2019-10-01 20:58:48 -0500

    Initial commit

---
+++ database/migrations/2019_10_01_020040_create_solicituds_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSolicitudsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solicituds', function (Blueprint $table) {
+            $table->bigIncrements('id');  
+            $table->string('nombre');
+            $table->string('documento');
+            $table->string('direccion');
+            $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('sexo');
+            $table->string('telefono');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicituds');
+    }
+}
